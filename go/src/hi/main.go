516d7075c41ca027ae01b4246a9f0ebb11e4ae03
georgi <remov.georgi@gmail.com>
2023-05-09 19:52:17 +0300

    new packages and files

--- go/src/hi/main.go
+++ go/src/hi/main.go
@@ -1,10 +1,34 @@
 package main
 
 import (
+
+	//"golang_lessons/go/src/keyboard"
 	"golang_lessons/go/src/greeting"
+	"golang_lessons/go/src/greeting/deutsch"
 )
 
 func main() {
+
+	//The commented code below is related to run function from the keyboard package:
+	// 	fmt.Print("Enter a grade:")
+	// 	grade, err := keyboard.GetFloat()
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+
+	// 	var status string
+
+	// 	if grade >= 60 {
+	// 		status = "passing"
+	// 	} else {
+	// 		status = "failing"
+	// 	}
+
+	// 	fmt.Println("A grade of", grade, "is", status)
+
 	greeting.Hello()
 	greeting.Hi()
+	deutsch.Hallo()
+	deutsch.GutenTag()
+
 }
