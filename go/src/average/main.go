24a3b1672bcf3bcbc56b631c513ccbdcb7ebc898
georgi <remov.georgi@gmail.com>
2023-05-29 19:30:47 +0300

    added chapter 6 code

--- go/src/average/main.go
+++ go/src/average/main.go
@@ -2,24 +2,45 @@ package main
 
 import (
 	"fmt"
-	"golang_lessons/go/src/datafile"
 	"log"
+	"os"
+	"strconv"
 )
 
-func main() {
-
-	numbers, err := datafile.GetFloats("data.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
+func average(numbers ...float64) float64 {
 	var sum float64 = 0
 	for _, number := range numbers {
 		sum += number
 	}
+	return sum / float64(len(numbers))
+}
 
-	fmt.Printf("Total for the last weeks is: %0.2f\n", sum)
+func main() {
 
-	sampleCount := float64(len(numbers))
-	fmt.Printf("Average: %0.2f\n", sum/sampleCount)
+	arguments := os.Args[1:]
+	var numbers []float64
+	for _, argument := range arguments {
+		number, err := strconv.ParseFloat(argument, 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		numbers = append(numbers, number)
+	}
+
+	fmt.Printf("Average: %0.2f\n", average(numbers...))
+
+	// numbers, err := datafile.GetFloats("data.txt")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// var sum float64 = 0
+	// for _, number := range numbers {
+	// 	sum += number
+	// }
+
+	// fmt.Printf("Total for the last weeks is: %0.2f\n", sum)
+
+	// sampleCount := float64(len(numbers))
+	// fmt.Printf("Average: %0.2f\n", sum/sampleCount)
 
 }
--- go/src/datafile/floats.go
+++ go/src/datafile/floats.go
@@ -9,22 +9,21 @@ import (
 )
 
 // GetFloats reads float64 from each line of a file
-func GetFloats(fileName string) ([3]float64, error) {
-	var numbers [3]float64
+func GetFloats(fileName string) ([]float64, error) {
+	var numbers []float64
 
 	file, err := os.Open(fileName)
 	if err != nil {
 		return numbers, err
 	}
-	i := 0
 	scanner := bufio.NewScanner(file)
 
 	for scanner.Scan() {
-		numbers[i], err = strconv.ParseFloat(scanner.Text(), 64)
+		number, err := strconv.ParseFloat(scanner.Text(), 64)
 		if err != nil {
 			return numbers, err
 		}
-		i++
+		numbers = append(numbers, number)
 	}
 
 	err = file.Close()
